6c21c3cbf0845028d5020189e2751dc5552e00d9
JackReiman <87156347+HobbitHoleJack@users.noreply.github.com>
2022-10-30 14:55:57 -0700

    better main.py

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
 import random
 import string
 import os
+import disablerootlogin.py as drl
 
+def passwordchanger():
+    filepath = "users.txt"
+    with open(filepath) as fp:
+        lines = fp.read().splitlines()
+    with open(filepath, "w") as fp:
+        for line in lines:
+            if '#' not in line:
+                print(line + ":" + passwordgen(), file=fp)
+
+    print("open up the users.txt file, are you ready to change their passwords? Y/N:")
+    run = input()
+    if run.lower() == 'y':
+        os.system("sudo chpasswd < users.txt")
 
 def passwordgen():
     password_length = 20
@@ -16,28 +30,39 @@ def passwordgen():
     return password
 
 def passwdenforcer():
+    
     os.system("sudo apt-get install libpam-cracklib")
     os.system("sudo gedit /etc/login.defs")
     os.system("sudo gedit /etc/pam.d/common-password")
 
-
-if __name__ == '__main__':
-    filepath = "users.txt"
-    with open(filepath) as fp:
-        lines = fp.read().splitlines()
-    with open(filepath, "w") as fp:
-        for line in lines:
-            if '#' not in line:
-                print(line + ":" + passwordgen(), file=fp)
-
-    print("open up the users.txt file, are you ready to change their passwords? Y/N:")
-    run = input()
-    if run.lower() == 'y':
-        os.system("sudo chpasswd < users.txt")
-    else:
-        exit()
-
-    passwdenforcer()
+def mp3finder():
     print("finding .mp3s")
     os.system("cd /home")
     os.system('sudo find / -iname "*.mp3" -print')
+
+
+if __name__ == '__main__':
+
+
+    print("Want to enforce password requirements? (this is all manual input, make sure you know what you're looking for) Y/N:")
+    run = input()
+    if run.lower() == 'y':
+        drl.disablerootlogin()
+
+    print("Want to enforce password requirements? (this is all manual input, make sure you know what you're looking for) Y/N:")
+    run = input()
+    if run.lower() == 'y':
+        passwdenforcer()
+
+    print("Want to change passwords? Y/N:")
+    run = input()
+    if run.lower() == 'y':
+        passwordchanger()
+
+    print("Want to find all mp3 files? Y/N:")
+    run = input()
+    if run.lower() == 'y':
+        mp3finder()
+
+
+    
