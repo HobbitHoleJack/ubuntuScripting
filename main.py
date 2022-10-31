ae0825ee862074e7d3ad2445d1487c8735ae42a8
JackReiman <87156347+HobbitHoleJack@users.noreply.github.com>
2022-10-31 11:39:29 -0700

    made the password enforcer slightly less dangerous

--- main.py
+++ main.py
@@ -7,29 +7,34 @@ import functions as func
 if __name__ == '__main__':
 
 
-    print("Want to enforce password requirements? (this is all manual input, make sure you know what you're looking for) Y/N:")
+    print("Want to disable ssh root login? Y/N:")
     run = input()
     if run.lower() == 'y':
         func.disablerootlogin()
+    run = ""
 
     print("Want to enforce password requirements? (extremely untested and dangerous) Y/N:")
     run = input()
     if run.lower() == 'y':
         func.passwdenforcer()
+    run = ""
 
     print("Want to change passwords? Y/N:")
     run = input()
     if run.lower() == 'y':
         func.passwordchanger()
+    run = ""
 
     print("Want to find all mp3 files? Y/N:")
     run = input()
     if run.lower() == 'y':
         func.mp3finder()
+    run = ""
 
     print("run updates? Y/N")
     run = input()
     if run.lower() == 'y':
         func.updates()
+    run = ""
 
     
